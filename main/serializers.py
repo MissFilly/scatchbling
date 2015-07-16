761050e0c6a23027d8ec94dd8e6c5e59546e9b9a
MissFilly <lady.filly@gmail.com>
2015-07-15 21:10:41 -0300

    Added TokenAuthentication

--- main/serializers.py
+++ main/serializers.py
@@ -1,9 +1,17 @@
 from rest_framework import serializers
-from .models import Item
+from .models import Item, Size
+
+
+class SizeListingField(serializers.RelatedField):
+    def to_representation(self, value):
+        return value.name
+
+    def to_internal_value(self, data):
+        return Size.objects.get(name=data)
 
 
 class ItemSerializer(serializers.ModelSerializer):
-    sizes = serializers.StringRelatedField(many=True)
+    sizes = SizeListingField(many=True, queryset=Size.objects.all())
 
     class Meta:
         model = Item
--- scatchbling/settings.py
+++ scatchbling/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/1.8/ref/settings/
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
+import dj_database_url
+
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -27,6 +29,8 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
 
 # Application definition
 
@@ -39,6 +43,7 @@ INSTALLED_APPS = (
     'django.contrib.staticfiles',
     # Third party applications
     'rest_framework',
+    'rest_framework.authtoken',
     # Project applications
     'main'
 )
@@ -79,10 +84,7 @@ WSGI_APPLICATION = 'scatchbling.wsgi.application'
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    'default': dj_database_url.config(default='sqlite:////{0}'.format(os.path.join(BASE_DIR, 'db.sqlite3')))
 }
 
 
@@ -104,3 +106,22 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATIC_ROOT = 'staticfiles'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.TokenAuthentication',
+    ),
+    'DEFAULT_RENDERER_CLASSES': (
+        'rest_framework.renderers.JSONRenderer',
+    ),
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated',
+
+    )
+}
